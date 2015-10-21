bb15167066ca44de4aef2b9678a2f2cecc584f05
Ilaria Bossi <bossiilaria@users.noreply.github.com>
2015-10-20 23:08:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,19 @@ myFunction(1, 3);
 */
 
 /*Goal: calculate how many lines of code I would write by the time I'm 50 if I write 10 lines of code a day*/
+/*
 var calcLinesOfCode = function(numYears) {
 	return 10 * 365 * numYears;
 };
 alert(calcLinesOfCode(8));
+*/
+/*Illustrate two different ways to define a function */
+var myFunc = function(a, b) {
+	return a*b;
+};
+console.log(myFunc(2, 3));
+
+function myFunc(a, b) {
+	return a*b;
+}
+console.log(myFunc(4, 5));
