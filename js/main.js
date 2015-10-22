4f9218157af67c0726b3d23dbba2606b8ed10751
Ilaria Bossi <bossiilaria@users.noreply.github.com>
2015-10-22 09:56:20 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-/*var myFunction = function(y, z){
+/* Goal: write a function to add two numbers and display the result*/
+
+/*
+var myFunction = function(y, z){
 alert(y + z);
 };
 
@@ -11,8 +14,9 @@ var calcLinesOfCode = function(numYears) {
 	return 10 * 365 * numYears;
 };
 alert(calcLinesOfCode(8));
-*/
-/*Illustrate two different ways to define a function */
+
+/* Goal: write a function to multiply two numbers and display the result*/
+/*
 var myFunc = function(a, b) {
 	return a*b;
 };
@@ -22,3 +26,21 @@ function myFunc(a, b) {
 	return a*b;
 }
 console.log(myFunc(4, 5));
+*/
+
+/*Goal: play with local and global variables - write a function to calculate how tall a person grew over the years and display the result*/
+var lifeTimeInches = 20;
+
+var howTall = function(startAge, endAge, inchesPerYear) {
+	totalInches = inchesPerYear * (endAge - startAge);
+	lifeTimeInches += totalInches;
+	return totalInches;
+};
+console.log("From 0 to 8 years:");
+console.log(howTall(0, 8, 2.5));
+
+console.log("From 8 to 16 years:");
+console.log(howTall(8, 16, 2));
+
+console.log("Total inches grown:");
+console.log(lifeTimeInches);
